7a1bf18b155caed0ea86a83dea18ebad87accb0b
Evgeny Gazdovsky <gazdovsky@gmail.com>
2011-04-12 10:45:25 +0400

    [feature] Add XQuery tests

---
+++ src/test/java/org/animotron/exist/XQueryTests.java
@@ -0,0 +1,11 @@
+package org.animotron.exist;
+
+import xquery.TestRunner;
+
+public class XQueryTests extends TestRunner {
+
+    @Override
+    protected String getDirectory() {
+        return "src/test/xquery";
+    }
+}
