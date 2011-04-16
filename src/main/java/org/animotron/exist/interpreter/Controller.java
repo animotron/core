2cbc01a1415f904d38d3d6ef823812ef4f668e45
Dmitriy Shabanov <shabanovd@gmail.com>
2011-04-16 23:02:56 +0500

    implement "preferedUse"

--- src/main/java/org/animotron/exist/interpreter/Controller.java
+++ src/main/java/org/animotron/exist/interpreter/Controller.java
@@ -18,10 +18,58 @@
  */
 package org.animotron.exist.interpreter;
 
+import org.animotron.exist.index.AnimoIndex;
+import org.animotron.exist.index.AnimoIndexWorker;
+import org.exist.Database;
+import org.exist.dom.NewArrayNodeSet;
+import org.exist.dom.NodeProxy;
+import org.exist.dom.NodeSet;
+
 /**
  * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
  *
  */
 public class Controller {
 
+	private Database db;
+	
+	public Controller(Database db) {
+		this.db = db;
+	}
+	
+	public AnimoIndexWorker getIndexWorker() {
+        return (AnimoIndexWorker) db.getActiveBroker().getIndexController().getWorkerByIndexId(AnimoIndex.ID);
+	}
+
+	public NodeSet getUse(NodeSet set) {
+		//XXX: code
+//      let $query := concat("unordered($CONTEXT/", local:name-sequence($is, "descendant-or-self::use"), ")")
+//      let $use   := local:eval-query($query, (), $context, ())
+		return set;
+	}
+	
+	//TODO: write tests
+	public NodeSet preferedUse(NodeSet is) {
+		if (is.isEmpty())
+			return NodeSet.EMPTY_SET;
+		
+		NodeSet result = new NewArrayNodeSet();
+		
+		NodeSet use = getUse(is);
+		for (NodeProxy node : use) {
+			NodeSet useIS = getIndexWorker().resolveDownIsLogic(node);
+
+			if (useIS.isEmpty())
+				continue;
+			
+			NodeSet res = use.intersection(useIS);
+			
+			if (res.isEmpty())
+				result.add(node);
+			else
+				result.addAll(res);
+		}
+		
+		return result;
+	}
 }
