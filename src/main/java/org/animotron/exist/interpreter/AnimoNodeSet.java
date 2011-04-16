a0b950267f31b634af481cf692472ebe9908007d
Evgeny Gazdovsky <gazdovsky@gmail.com>
2011-04-16 12:43:11 +0400

    new xquery functions

---
+++ src/main/java/org/animotron/exist/interpreter/AnimoNodeSet.java
@@ -0,0 +1,79 @@
+package org.animotron.exist.interpreter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.saxon.type.Type;
+
+import org.animotron.Namespaces;
+import org.exist.dom.ElementImpl;
+import org.exist.dom.NewArrayNodeSet;
+import org.exist.dom.NodeProxy;
+import org.exist.dom.NodeSet;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class AnimoNodeSet {
+	
+	private NodeSet set;
+	
+	private Map <String, NodeSet> instances = new HashMap <String, NodeSet> ();
+	private Map <String, NodeSet> properties = new HashMap <String, NodeSet> ();
+	
+	public AnimoNodeSet(NodeSet set){
+		this.set = set;
+		scan(set);
+	}
+	
+	public NodeSet getNodes(){
+		return set;
+	}
+	
+	private void scan(ElementImpl node, Map <String, NodeSet> map){
+		String name = node.getLocalName();
+		NodeSet set = map.get(name);
+		if (set == null) {
+			set = new NewArrayNodeSet();
+			map.put(name, set);
+		}
+		NodeProxy proxy = new NodeProxy(node);
+		set.add(proxy);
+	}
+	
+	private void scan(ElementImpl node){
+		String ns = node.getNamespaceURI();
+		if (Namespaces.THE.equals(ns)){
+			scan(node, instances);
+		} else if (Namespaces.HAVE.equals(ns) || Namespaces.IC.equals(ns)){
+			scan(node, properties);
+		}
+		scan(node.getChildNodes());
+	}
+	
+	private void scan(NodeSet set){
+		for (NodeProxy i : set){
+			if (i.getType() == Type.ELEMENT){
+				ElementImpl node = (ElementImpl) i.getNode();
+				scan(node);
+			}
+		}
+	}
+	
+	private void scan(NodeList set){
+		for (int i = 0; i < set.getLength(); i++){
+			Node node = set.item(i);
+			if (node.getNodeType() == Type.ELEMENT){
+				scan((ElementImpl)node);
+			}
+		}
+	}
+	
+	public NodeSet resolveReference(String name){
+		return instances.get(name); 
+	}
+	
+	public NodeSet getProperty(String name){
+		return properties.get(name); 
+	}
+	
+}
---
+++ src/main/java/org/animotron/exist/xquery/functions/ResolveReference.java
@@ -0,0 +1,93 @@
+/*
+ *  Copyright (C) 2011 The Animo Project
+ *  http://animotron.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 3
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.animotron.exist.xquery.functions;
+
+import org.animotron.exist.index.AnimoIndex;
+import org.animotron.exist.index.AnimoIndexWorker;
+import org.animotron.exist.interpreter.AnimoNodeSet;
+import org.exist.dom.NodeSet;
+import org.exist.dom.QName;
+import org.exist.xquery.BasicFunction;
+import org.exist.xquery.Cardinality;
+import org.exist.xquery.FunctionSignature;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQueryContext;
+import org.exist.xquery.value.FunctionParameterSequenceType;
+import org.exist.xquery.value.FunctionReturnSequenceType;
+import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.SequenceIterator;
+import org.exist.xquery.value.SequenceType;
+import org.exist.xquery.value.Type;
+import org.exist.xquery.value.ValueSequence;
+
+/**
+ * @author <a href="mailto:gazdovsky@gmail.com">Evgeny Gazdovsky</a>
+ * 
+ */
+public class ResolveReference extends BasicFunction {
+	
+	private static String NAME = "resolve-reference"; 
+	
+	private static SequenceType ARG1 = 
+		new FunctionParameterSequenceType("name", Type.STRING, Cardinality.ONE_OR_MORE, "The name of instance for resolving");
+	
+	private static SequenceType ARG2 = 
+		new FunctionParameterSequenceType("source", Type.NODE, Cardinality.ZERO_OR_MORE, "The source for resolving");
+	
+	private static FunctionReturnSequenceType RES = 
+		new FunctionReturnSequenceType(Type.NODE, Cardinality.ZERO_OR_MORE, "Returns properties nodes");
+
+	public final static FunctionSignature[] signature = {
+		new FunctionSignature(
+				new QName(NAME, AnimoModule.NAMESPACE_URI, AnimoModule.PREFIX),
+				"Resolve reference in global context",
+				new SequenceType[] {ARG1}, RES),
+		new FunctionSignature(
+				new QName(NAME, AnimoModule.NAMESPACE_URI, AnimoModule.PREFIX),
+				"Resolve properties in input source",
+				new SequenceType[] {ARG1, ARG2}, RES)
+	};
+	
+	public ResolveReference(XQueryContext context, FunctionSignature signature) {
+		super(context, signature);
+	}
+
+	@Override
+	public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException {
+		SequenceIterator i = args[0].iterate();
+		Sequence res = new ValueSequence();
+		if (args.length == 1){
+			AnimoIndexWorker wk = (AnimoIndexWorker) context.getBroker().getIndexController().getWorkerByIndexId(AnimoIndex.ID);
+			while (i.hasNext()){
+				String name = i.nextItem().getStringValue();
+				NodeSet set = wk.getNode(name);
+				res.addAll(set);
+			}
+		} else {
+			AnimoNodeSet source = new AnimoNodeSet(args[1].toNodeSet());
+			while (i.hasNext()){
+				String name = i.nextItem().getStringValue();
+				NodeSet set = source.resolveReference(name);
+				res.addAll(set);
+			}
+		}
+		return res;
+	}
+	
+}
