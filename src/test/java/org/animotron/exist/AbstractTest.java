743c6d024e6b15cbbb23215df15206af6f2ddd94
Dmitriy Shabanov <shabanovd@gmail.com>
2011-05-14 12:57:34 +0500

    add 'query' ns

--- src/test/java/org/animotron/exist/AbstractTest.java
+++ src/test/java/org/animotron/exist/AbstractTest.java
@@ -56,7 +56,10 @@ public class AbstractTest {
 		"xmlns:an='animo/reference' " +
 		"xmlns:ptrn='animo/pattern' " +
 		"xmlns:have='animo/relation/have' " +
-		"xmlns:is='animo/relation/is'";
+		"xmlns:is='animo/relation/is'" +
+		"xmlns:get='animo/query/extract'" +
+		"xmlns:any='animo/query/any'" +
+		"xmlns:all='animo/query/all'";
 	
 	protected DocumentSet configureAndStore(String configuration, Map<String, String> nameDataMap) {
         DBBroker broker = null;
