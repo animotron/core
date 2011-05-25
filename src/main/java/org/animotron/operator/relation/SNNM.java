5e6f59ed55c6db029bc120f6e2ac03bda5d21831
Dmitriy Shabanov <shabanovd@gmail.com>
2011-05-25 19:54:28 +0500

    adding "snnm" - "animo/relation/synonym" operator

---
+++ src/main/java/org/animotron/operator/relation/SNNM.java
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (C) 2011 The Animo Project
+ *  http://animotron.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 3
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.animotron.operator.relation;
+
+import org.animotron.operator.Operator;
+
+/**
+ * Operator 'SNNM'.
+ * 
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ * @author <a href="mailto:gazdovsky@gmail.com">Evgeny Gazdovsky</a>
+ */
+public class SNNM extends Operator {
+	
+	public static final SNNM INSTANCE = new SNNM();
+	public static SNNM getInstance() { return INSTANCE; }
+	
+	private SNNM() { super("snnm", "animo/relation/synonym"); }
+
+}
