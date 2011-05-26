676e9e433fce431b72b8133270a5f69cb0acd706
Dmitriy Shabanov <shabanovd@gmail.com>
2011-05-26 21:15:23 +0500

    new query operator
    it can be used in another query operator 
    
    example: all:A has:B eq:C an:Z

---
+++ src/main/java/org/animotron/operator/query/HAS.java
@@ -0,0 +1,37 @@
+/*
+ *  Copyright (C) 2011 The Animo Project
+ *  http://animotron.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 3
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.animotron.operator.query;
+
+import org.animotron.operator.Query;
+import org.animotron.operator.Reference;
+
+/**
+ * Query operator 'HAS'.
+ * 
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ * @author <a href="mailto:gazdovsky@gmail.com">Evgeny Gazdovsky</a>
+ */
+public class HAS extends Reference implements Query {
+	
+	private static final HAS INSTANCE = new HAS();
+	public static HAS getInstance() { return INSTANCE; }
+	
+	private HAS() { super("has", "animo/query/has"); }
+	
+}
