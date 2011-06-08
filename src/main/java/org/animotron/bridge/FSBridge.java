e4bb7b90715d76d67b3dd1e5c91884c3899441c7
Evgeny Gazdovsky <gazdovsky@gmail.com>
2011-06-08 22:59:22 +0400

    refactor the core project, add an animo sources

---
+++ src/main/java/org/animotron/bridge/FSBridge.java
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (C) 2011 The Animo Project
+ *  http://animotron.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 3
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.animotron.bridge;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.animotron.graph.CommonGraphBuilder;
+
+/**
+ * Repository loader
+ * 
+ * @author <a href="mailto:gazdovsky@gmail.com">Evgeny Gazdovsky</a>
+ *
+ */
+public class FSBridge {
+	
+	public static void load (String path) throws XMLStreamException, FileNotFoundException {
+		load(new File(path));
+	}
+	
+	public static void load (File path) throws XMLStreamException, FileNotFoundException {
+		
+		if (path.isDirectory()) {
+			for (File file : path.listFiles()) {
+				load(file);
+			}
+		} else {
+			CommonGraphBuilder.build(path);
+		}
+		
+	}
+	
+}
