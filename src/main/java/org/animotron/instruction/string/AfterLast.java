5bc3cf256201809fb753daeb8e1227e3b0abd0e1
Dmitriy Shabanov <shabanovd@gmail.com>
2011-06-11 13:31:56 +0500

    String container & after-last instruction

---
+++ src/main/java/org/animotron/instruction/string/AfterLast.java
@@ -0,0 +1,61 @@
+/*
+ *  Copyright (C) 2011 The Animo Project
+ *  http://animotron.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 3
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.animotron.instruction.string;
+
+import java.io.IOException;
+
+import org.animotron.graph.AnimoGraph;
+import org.animotron.instruction.AbstractInstruction;
+import org.animotron.io.PipedOutputObjectStream;
+import org.animotron.serializer.StringSerializer;
+import org.neo4j.graphdb.Relationship;
+
+/**
+ * String instruction 'after-last'.
+ * 
+ * Return last chars from input string after last-found defined pattern.
+ * 
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ */
+public class AfterLast extends AbstractInstruction {
+	
+	public static final AfterLast _ = new AfterLast();
+	
+	private AfterLast() { super("after-last", STRING._); }
+	
+	public void eval(Relationship op, PipedOutputObjectStream ot, boolean isLast) throws IOException {
+		
+		//UNDERSTAND: if we have more that 2 params, what to do?
+		
+		Relationship[] params = AnimoGraph.getORDER().first(2, op.getEndNode());
+		
+		//pattern
+		StringSerializer szer = new StringSerializer();
+		szer.serialize(params[0]);
+		String pattern = szer.getString();
+
+		szer.serialize(params[1]);
+		String source = szer.getString();
+		
+		int index = source.lastIndexOf(pattern);
+		String result = source.substring(index);
+		
+		System.out.println(result);
+	}
+}
