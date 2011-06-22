d2df815d24917868e42ed39f8b642caa852f96ce
Dmitriy Shabanov <shabanovd@gmail.com>
2011-06-22 13:21:45 +0500

    objects pool

---
+++ src/main/java/org/animotron/utils/ObjectPool.java
@@ -0,0 +1,85 @@
+/*
+ *  Copyright (C) 2011 The Animo Project
+ *  http://animotron.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 3
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.animotron.utils;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+/**
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ * 
+ */
+public abstract class ObjectPool<T> {
+	private long expirationTime;
+
+	private Hashtable<T, Long> locked, unlocked;
+
+	public ObjectPool() {
+		this(Long.MAX_VALUE);
+	}
+
+	public ObjectPool(long seconds) {
+		expirationTime = seconds  * 1000;
+		locked = new Hashtable<T, Long>();
+		unlocked = new Hashtable<T, Long>();
+	}
+
+	protected abstract T create();
+
+	public abstract boolean validate(T o);
+
+	public abstract void expire(T o);
+
+	public synchronized T checkOut() {
+		long now = System.currentTimeMillis();
+		T t;
+		if (unlocked.size() > 0) {
+			Enumeration<T> e = unlocked.keys();
+			while (e.hasMoreElements()) {
+				t = e.nextElement();
+				if ((now - unlocked.get(t)) > expirationTime) {
+					// object has expired
+					unlocked.remove(t);
+					expire(t);
+					t = null;
+				} else {
+					if (validate(t)) {
+						unlocked.remove(t);
+						locked.put(t, now);
+						return (t);
+					} else {
+						// object failed validation
+						unlocked.remove(t);
+						expire(t);
+						t = null;
+					}
+				}
+			}
+		}
+		// no objects available, create a new one
+		t = create();
+		locked.put(t, now);
+		return (t);
+	}
+
+	public synchronized void checkIn(T t) {
+		locked.remove(t);
+		unlocked.put(t, System.currentTimeMillis());
+	}
+}
