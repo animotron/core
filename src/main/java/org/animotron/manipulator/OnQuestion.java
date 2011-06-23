c6b093e13c8377c9363ab1bb361272b6ebe77b7b
Dmitriy Shabanov <shabanovd@gmail.com>
2011-06-23 13:01:51 +0500

    OnQuestion common part

---
+++ src/main/java/org/animotron/manipulator/OnQuestion.java
@@ -0,0 +1,59 @@
+/*
+ *  Copyright (C) 2011 The Animo Project
+ *  http://animotron.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 3
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.animotron.manipulator;
+
+import static org.neo4j.graphdb.Direction.OUTGOING;
+
+import java.util.Iterator;
+
+import org.animotron.Executor;
+import org.jetlang.channels.Subscribable;
+import org.jetlang.core.DisposingExecutor;
+import org.neo4j.graphdb.Relationship;
+
+/**
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ *
+ */
+public class OnQuestion implements Subscribable<PFlow> {
+	
+	@Override
+	public void onMessage(PFlow pf) {
+		int count = 0;
+		Iterator<Relationship> it = pf.getOP().getEndNode().getRelationships(OUTGOING).iterator();
+		while (it.hasNext()) {
+			Relationship r = it.next();
+			Subscribable<PFlow> onQuestion = pf.getManipulator().onQuestion(r);
+			
+			PFlow nextPF = new PFlow(pf, r);
+			nextPF.question.subscribe(onQuestion);
+			
+			nextPF.question.publish(nextPF);
+			
+			count++;
+		}
+	}
+
+	@Override
+	public DisposingExecutor getQueue() {
+		System.out.println("getQueue");
+		return Executor.getFiber();
+	}
+
+}
