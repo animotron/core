5b938eafb80fc25f2be87fdbc12e2dc5f552d17f
Evgeny Gazdovsky <gazdovsky@gmail.com>
2011-07-04 15:14:18 +0400

    simpler traversal description

--- src/main/java/org/animotron/operator/relation/IS.java
+++ src/main/java/org/animotron/operator/relation/IS.java
@@ -37,7 +37,6 @@ import org.neo4j.graphdb.traversal.TraversalDescription;
 import org.neo4j.graphdb.traversal.Traverser;
 import org.neo4j.helpers.Predicate;
 import org.neo4j.kernel.Traversal;
-import org.neo4j.kernel.Uniqueness;
 
 /**
  * Operator 'IS'.
@@ -55,7 +54,6 @@ public class IS extends Relation implements Prepare {
 		super("is", "animo/relation/is"); 
 		TD = Traversal.description()
 			.depthFirst()
-			.uniqueness(Uniqueness.RELATIONSHIP_PATH)
 			.relationships(relationshipType(), OUTGOING)
 			.evaluator(Evaluators.fromDepth(1));
 	}
