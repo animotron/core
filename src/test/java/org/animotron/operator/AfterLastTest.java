e39b6687286e4b41be3e9fea0f274e64cb4d9740
Evgeny Gazdovsky <gazdovsky@gmail.com>
2011-07-19 11:59:39 +0400

    AfterLastTest

---
+++ src/test/java/org/animotron/operator/AfterLastTest.java
@@ -0,0 +1,53 @@
+/*
+ *  http://animotron.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 3
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.animotron.operator;
+
+import org.animotron.ATest;
+import org.animotron.Expression;
+import org.animotron.instruction.string.AfterLast;
+import org.animotron.operator.query.GET;
+import org.animotron.operator.relation.HAVE;
+import org.junit.Test;
+
+import static org.animotron.Expression._;
+import static org.animotron.Expression.text;
+
+/**
+ * @author <a href="mailto:gazdovsky@gmail.com">Evgeny Gazdovsky</a>
+ *
+ */
+public class AfterLastTest extends ATest {
+	
+	@Test
+	public void testAfterLast() throws Exception {
+        System.out.println("Test 'AfterLast' ...");
+        
+    	new Expression(
+			_(THE._, "A", _(HAVE._, "B", text("x.y.z")))
+		);
+    	
+    	Expression C = new Expression(
+			_(THE._, "C", _(AfterLast._, _(GET._, "B", _(AN._, "A"))))
+		);
+    	
+        assertAnimo(C, "<the:C><have:B>c</have:B></the:C>");
+
+        //System.out.println("done.");
+	}
+	
+}
