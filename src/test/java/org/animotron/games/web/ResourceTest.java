592c7b6cb9e802474a9868ab838ff2b94dd09888
Evgeny Gazdovsky <gazdovsky@gmail.com>
2011-08-14 14:39:43 +0400

    yet another web test

---
+++ src/test/java/org/animotron/games/web/ResourceTest.java
@@ -0,0 +1,87 @@
+/*
+ *  Copyright (C) 2011 The Animo Project
+ *  http://animotron.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 3
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.animotron.games.web;
+
+import org.animotron.ATest;
+import org.animotron.Expression;
+import org.animotron.exception.EBuilderTerminated;
+import org.animotron.operator.AN;
+import org.animotron.operator.THE;
+import org.animotron.operator.query.ANY;
+import org.animotron.operator.relation.HAVE;
+import org.animotron.operator.relation.IS;
+import org.animotron.operator.relation.USE;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.animotron.Expression._;
+import static org.animotron.Expression.text;
+
+/**
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ * @author <a href="mailto:gazdovsky@gmail.com">Evgeny Gazdovsky</a>
+ *
+ */
+public class ResourceTest extends ATest {
+
+	@Test
+	public void test() throws EBuilderTerminated, IOException, InterruptedException {
+
+    	new Expression(
+			_(THE._, "service",
+				_(IS._, "resource")
+			)
+		);
+
+        new Expression(
+            _(THE._, "root-service",
+                _(IS._, "service"),
+                _(IS._, "root"),
+                _(HAVE._, "title", text("root"))
+            )
+        );
+
+        new Expression(
+            _(THE._, "not-found-service",
+                _(IS._, "service"),
+                _(IS._, "not-found"),
+                _(HAVE._, "title", text("root"))
+            )
+        );
+
+        new Expression(
+            _(THE._, "rest",
+                _(ANY._, "resource")
+            )
+        );
+
+        Expression s = new Expression(
+            _(THE._, "s",
+                _(AN._, "rest",
+                    _(USE._, "root")
+                )
+            )
+        );
+
+        assertAnimo(s,  "<the:s><the:rest><the:root-service><is:service/><is:root/><have:title>root</have:title></the:root-service></the:rest></the:s>");
+
+	}
+
+}
