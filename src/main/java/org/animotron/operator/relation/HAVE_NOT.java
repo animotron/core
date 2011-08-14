96886dcfd884721e42a80a34d723f10d41e55ae4
Dmitriy Shabanov <shabanovd@gmail.com>
2011-08-14 14:22:59 +0500

    HAVE NOT & IS NOT relations

---
+++ src/main/java/org/animotron/operator/relation/HAVE_NOT.java
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (C) 2011 The Animo Project
+ *  http://animotron.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 3
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.animotron.operator.relation;
+
+import org.animotron.operator.AbstractOperator;
+import org.animotron.operator.Cachable;
+import org.animotron.operator.Property;
+
+/**
+ * Operator 'HAVE-NOT'.
+ * 
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ * @author <a href="mailto:gazdovsky@gmail.com">Evgeny Gazdovsky</a>
+ */
+public class HAVE_NOT extends AbstractOperator implements Cachable, Property {
+	
+	public static final HAVE_NOT _ = new HAVE_NOT();
+
+	private HAVE_NOT() { super("have-not", "animo/relation/have-not"); }
+}
