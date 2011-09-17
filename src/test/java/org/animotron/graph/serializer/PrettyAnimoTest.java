2b1f40190fb0008258d1c59cbd671c835f71cb6f
Evgeny Gazdovsky <gazdovsky@gmail.com>
2011-09-17 17:51:29 +0400

    Pretty animo serializer

--- src/test/java/org/animotron/graph/serializer/PrettyAnimoTest.java
+++ src/test/java/org/animotron/graph/serializer/PrettyAnimoTest.java
@@ -90,10 +90,29 @@ public class PrettyAnimoTest extends ATest {
 
     @Test
     public void test_09() throws IOException, AnimoException {
-        test("the a \\b \\c\n" +
-             "            @d\n" +
-             "                (e)\n" +
-             "                (f)");
+        test(   "the a \\b \\c\n" +
+                "            @d\n" +
+                "                (e)\n" +
+                "                (f)");
+    }
+
+    @Test
+    public void test_0A() throws IOException, AnimoException {
+        test(   "the rest have content\n" +
+                "        \\html\n" +
+                "            (\\head \\title have title \"Welcome to Animo\")\n" +
+                "            (\\body\n" +
+                "                theme-concrete-root-layout\n" +
+                "                    (is root-layout)\n" +
+                "                    (\\h1 have title \"Welcome to Animo\")\n" +
+                "                    (\\p have content \"It is working\")\n" +
+                "                    (\\ul\n" +
+                "                        (\\li\n" +
+                "                            (\"host:\")\n" +
+                "                            (\\strong have host \"localhost\"))\n" +
+                "                        (\\li\n" +
+                "                            (\"uri:\")\n" +
+                "                            (\\strong have uri \"/\"))))");
     }
 
 }
