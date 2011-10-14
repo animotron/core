0dd5d6270d0253ae2e2cc748f7b302cb3cbb724e
Dmitriy Shabanov <shabanovd@gmail.com>
2011-10-14 16:22:14 +0500

    future test idea

---
+++ src/test/java/org/animotron/games/words/TasksTest.java
@@ -0,0 +1,18 @@
+package org.animotron.games.words;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class TasksTest {
+
+	@Test
+	@Ignore
+	public void test() {
+		String s = "If Mary has 5 sheep and the wolf kills 2. How many sheep Mary has?";
+		s = "(Mary has) 5 sheep. kills 2. (How many) sheep (Mary has)?";
+		fail("Not yet implemented");
+	}
+
+}
