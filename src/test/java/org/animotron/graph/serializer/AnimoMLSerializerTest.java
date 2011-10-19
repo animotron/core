da591717763bb348cdc110e364ce558271720cd3
evgeny <evgeny@evgeny-laptop.(none)>
2011-10-20 01:07:58 +0400

    more assertions

--- src/test/java/org/animotron/graph/serializer/AnimoMLSerializerTest.java
+++ src/test/java/org/animotron/graph/serializer/AnimoMLSerializerTest.java
@@ -23,9 +23,12 @@ import org.animotron.expression.AnimoExpression;
 import org.animotron.expression.JExpression;
 import org.animotron.statement.operator.AN;
 import org.animotron.statement.operator.THE;
+import org.junit.Assert;
 import org.junit.Test;
 
+import static org.animotron.Properties.HASH;
 import static org.animotron.expression.JExpression.*;
+import static org.animotron.graph.Cache.key;
 
 
 /**
@@ -36,6 +39,7 @@ public class AnimoMLSerializerTest extends ATest {
 
     private void test(String in, String out) throws Exception {
         AnimoExpression expression = new AnimoExpression(in);
+        Assert.assertEquals(HASH.get(expression), key(DigestSerializer.serialize(expression)));
         assertAnimoResult(expression, out);
     }
 
