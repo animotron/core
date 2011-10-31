da928ddbcce43b8ae1ba5b0808dbce6b64e8584e
evgeny <evgeny@evgeny-laptop.(none)>
2011-10-31 17:30:14 +0400

    first attempt

--- src/main/java/org/animotron/statement/operator/combinator/EACH.java
+++ src/main/java/org/animotron/statement/operator/combinator/EACH.java
@@ -18,12 +18,16 @@
  */
 package org.animotron.statement.operator.combinator;
 
-import org.animotron.expression.Expression;
 import org.animotron.expression.JExpression;
 import org.animotron.graph.OrderIndex;
 import org.animotron.manipulator.OnQuestion;
 import org.animotron.manipulator.PFlow;
+import org.animotron.statement.Statement;
+import org.animotron.statement.Statements;
 import org.animotron.statement.link.LINK;
+import org.animotron.statement.ml.MLOperator;
+import org.animotron.statement.operator.AN;
+import org.animotron.statement.operator.THE;
 import org.jetlang.channels.Subscribable;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.index.IndexHits;
@@ -55,51 +59,66 @@ public class EACH extends Combinator {
 			System.out.println("EACH");
 			
 			LinkedList<Relationship> sets = new LinkedList<Relationship>();
-			
+
 			IndexHits<Relationship> elements = OrderIndex.queryDown(pf.getOPNode());
 			try {
 				while (elements.hasNext()) {
 					sets.add(elements.next());
 				}
-				
+
 				process(pf, sets, 1, null);
-				
+
 			} finally {
 				elements.close();
 			}
-			
-			
+
+
 			pf.done();
 		}
 	};
-	
+
 	private void process(PFlow pf, LinkedList<Relationship> sets, int pos, Relationship[] res) {
 		if (pos > sets.size()) {
-			
+
 			System.out.println(Arrays.toString(res));
-			
-			//XXX: hack required!
-			
-			Object[] obj = null;
-			for (int i = res.length-1; i >= 0; i--) {
-				if (obj == null)
-					obj = new Object[] {LINK._, null, new Object[] {res[i]}};
-				else {
-					obj = new Object[] {LINK._, null, new Object[] {res[i]}, obj};
-				}
-			}
-			pf.sendAnswer( Expression.__(new JExpression(obj)) );
+
+            Relationship r = res[res.length-1];
+            Relationship the = null;
+			for (int i = res.length-2; i >= 0; i--) {
+                Statement s = Statements.relationshipType(res[i]);
+                if (s instanceof MLOperator) {
+                    // TODO analyze more complex expressions
+                    IndexHits<Relationship> subelements = OrderIndex.queryDown(res[i].getEndNode());
+                    Object[][] bind = new Object[subelements.size() + 1][];
+                    try {
+                        int n = 0;
+                        for (Relationship x : subelements) {
+                            bind[n] = JExpression._(x);
+                            n++;
+                        }
+                        bind[n] = JExpression._(r);
+                        r = new JExpression(JExpression._(s, s.reference(res[i]), bind));
+                    } finally {
+                        subelements.close();
+                    }
+                } else {
+                    the = new JExpression(JExpression._(THE._, JExpression._(res[i])));
+                    r = new JExpression(JExpression._(AN._, the, JExpression._(r)));
+                }
+            }
+
+			pf.sendAnswer(r);
 			return;
 		}
-		
+
 		Relationship[] rs = new Relationship[pos];
-		
+
 		if (pos > 1) {
 			System.arraycopy(res, 0, rs, 0, res.length);
 		}
-		
+
 		Relationship rship = sets.get( sets.size()-pos );
-		
+
 		if (rship.isType(LINK._)) {
 			IndexHits<Relationship> subelements = OrderIndex.queryDown(rship.getEndNode());
 			try {
