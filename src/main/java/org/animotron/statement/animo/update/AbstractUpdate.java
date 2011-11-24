73f627aa2facd306566f2ae03bbc634a791635f2
evgeny <evgeny@evgeny-laptop.(none)>
2011-11-25 00:46:19 +0400

    prepare update statements

---
+++ src/main/java/org/animotron/statement/animo/update/AbstractUpdate.java
@@ -0,0 +1,46 @@
+/*
+ *  Copyright (C) 2011 The Animo Project
+ *  http://animotron.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 3
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.animotron.statement.animo.update;
+
+import org.animotron.manipulator.OnQuestion;
+import org.animotron.manipulator.PFlow;
+import org.animotron.statement.operator.Evaluable;
+import org.animotron.statement.operator.Operator;
+
+/**
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ * @author <a href="mailto:gazdovsky@gmail.com">Evgeny Gazdovsky</a>
+ */
+public abstract class AbstractUpdate extends Operator implements Evaluable {
+
+	protected AbstractUpdate(String name) { super(name); }
+	
+    @Override
+	public OnQuestion onCalcQuestion() {
+		return question;
+	}
+	
+	private OnQuestion question = new OnQuestion() {
+		@Override
+		public void onMessage(final PFlow pf) {
+			pf.done();
+		}
+	};
+	
+}
