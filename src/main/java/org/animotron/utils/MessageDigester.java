9dff0a7fbe6973adc2c3d7cc8dbaac8579f6602e
Dmitriy Shabanov <shabanovd@gmail.com>
2012-01-08 02:45:01 +0500

    update files header

--- src/main/java/org/animotron/utils/MessageDigester.java
+++ src/main/java/org/animotron/utils/MessageDigester.java
@@ -1,26 +1,24 @@
-package org.animotron.utils;
 /*
- *  eXist Open Source Native XML Database
- *  Copyright (C) 2010-2011 The eXist Project
- *  http://exist-db.org
- *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
+ *  Copyright (C) 2011-2012 The Animo Project
+ *  http://animotron.org
+ *  	
+ *  This file is part of Animotron.
+ *  
+ *  Animotron is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as 
+ *  published by the Free Software Foundation, either version 3 of 
+ *  the License, or (at your option) any later version.
+ *  
+ *  Animotron is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
- *  $Id: MessageDigester.java 13770 2011-02-12 18:43:33Z shabanovd $
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of 
+ *  the GNU Affero General Public License along with Animotron.  
+ *  If not, see <http://www.gnu.org/licenses/>.
  */
-
+package org.animotron.utils;
 
 import org.animotron.statement.Statement;
 import org.apache.log4j.Logger;
@@ -34,8 +32,8 @@ import static org.animotron.graph.Properties.NAME;
 
 public class MessageDigester {
 
-    private static String[] hex = {"0", "1", "2", "3", "4", "5", "6", "7",
-            "8", "9", "a", "b", "c", "d", "e", "f"};
+    private static String[] hex = 
+    	{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
 
     private static final Logger LOG = Logger.getLogger(MessageDigester.class);
 
@@ -195,4 +193,24 @@ public class MessageDigester {
     }
 
 }
-
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2010-2011 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  $Id: MessageDigester.java 13770 2011-02-12 18:43:33Z shabanovd $
+ */
