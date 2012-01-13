55b0d229cc3f8023e22e3df5f48fe95642f4eac0
Dmitriy Shabanov <shabanovd@gmail.com>
2012-01-13 16:01:50 +0500

    WEAK-USE operator

---
+++ src/main/java/org/animotron/statement/relation/WEAK_USE.java
@@ -0,0 +1,38 @@
+/*
+ *  Copyright (C) 2011-2012 The Animo Project
+ *  http://animotron.org
+ *  	
+ *  This file is part of Animotron.
+ *  
+ *  Animotron is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as 
+ *  published by the Free Software Foundation, either version 3 of 
+ *  the License, or (at your option) any later version.
+ *  
+ *  Animotron is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of 
+ *  the GNU Affero General Public License along with Animotron.  
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.animotron.statement.relation;
+
+
+import org.animotron.statement.operator.Operator;
+
+/**
+ * Operator 'WEAK-USE'.
+ * 
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ * @author <a href="mailto:gazdovsky@gmail.com">Evgeny Gazdovsky</a>
+ */
+public class WEAK_USE extends Operator {
+	
+	public static final WEAK_USE _ = new WEAK_USE();
+	
+	private WEAK_USE() { super("weak-use"); }
+
+}
