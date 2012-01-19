3c32c5be5f990cb46d988fcebfb336db174b8704
evgeny <evgeny@evgeny-laptop.(none)>
2012-01-19 17:29:54 +0400

    test get via any by an from any under common root

---
+++ src/test/java/org/animotron/statement/operator/AnyAnAnyGetTest.java
@@ -0,0 +1,41 @@
+/*
+ *  Copyright (C) 2011-2012 The Animo Project
+ *  http://animotron.org
+ *  	
+ *  This file is part of Animotron.
+ *  
+ *  Animotron is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as 
+ *  published by the Free Software Foundation, either version 3 of 
+ *  the License, or (at your option) any later version.
+ *  
+ *  Animotron is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of 
+ *  the GNU Affero General Public License along with Animotron.  
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.animotron.statement.operator;
+
+import org.animotron.ATest;
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ * @author <a href="mailto:gazdovsky@gmail.com">Evgeny Gazdovsky</a>
+ *
+ */
+public class AnyAnAnyGetTest extends ATest {
+
+    @Test
+    public void test() throws Exception {
+        testAnimo("the xxx-site (site) (name \"XXX\").");
+        testAnimo("the html-page any layout.");
+        testAnimo("the xxx-layout (layout) (get name).");
+        testAnimo("the xxx (any site) (html-page).");
+        assertStringResult("xxx", "XXX");
+    }
+}
