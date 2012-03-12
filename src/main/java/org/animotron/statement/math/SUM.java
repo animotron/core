3e54a9496a0ef07d44c116d89d52aa8ccf4b8173
Dmitriy Shabanov <shabanovd@gmail.com>
2012-03-12 16:55:37 +0500

    fix

--- src/main/java/org/animotron/statement/math/SUM.java
+++ src/main/java/org/animotron/statement/math/SUM.java
@@ -124,8 +124,12 @@ public class SUM extends MathInstruction {
 			Number Nb = VALUE.number(VALUE._.reference(b));
 
 			Number result;
-			if (Na instanceof Long && Nb instanceof Long) {
+			if (Na instanceof Integer && Nb instanceof Integer) {
+				result = Na.intValue() + Nb.intValue();
+			
+			} else if (Na instanceof Long && Nb instanceof Long) {
 				result = Na.longValue() + Nb.longValue();
+			
 			} else {
 				result = Na.doubleValue() + Nb.doubleValue();
 			}
