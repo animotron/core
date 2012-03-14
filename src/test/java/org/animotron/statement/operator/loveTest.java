93fbad0d45322ea8fbb329ed711d94d491120235
evgeny <evgeny@evgeny-laptop.(none)>
2012-03-14 17:01:11 +0400

    safe

---
+++ src/test/java/org/animotron/statement/operator/loveTest.java
@@ -0,0 +1,49 @@
+/*
+ *  Copyright (C) 2011-2012 The Animo Project
+ *  http://animotron.org
+ *  	
+ *  This file is part of Animotron.
+ *  
+ *  Animotron is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as 
+ *  published by the Free Software Foundation, either version 3 of 
+ *  the License, or (at your option) any later version.
+ *  
+ *  Animotron is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of 
+ *  the GNU Affero General Public License along with Animotron.  
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.animotron.statement.operator;
+
+import org.animotron.ATest;
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ * @author <a href="mailto:gazdovsky@gmail.com">Evgeny Gazdovsky</a>
+ *
+ */
+public class loveTest extends ATest {
+	
+    @Test
+    public void test() throws Throwable {
+        testAnimo("the Peter person.");
+        testAnimo("the Kate person.");
+        testAnimo("the Joe person.");
+        testAnimo("the John person.");
+        testAnimo("the Lily person.");
+        testAnimo("the Lui person.");
+        testAnimo("the event1 (Peter love Kate) (date \"2012-03-14\").");
+        testAnimo("the event2 (John love Joe) (date sometime).");
+        testAnimo("the event3 (Joe love Lily) (date \"2012-03-12\").");
+        testAnimo("the event4 (Lily love Lui) (date \"2012-03-15\").");
+        testAnimo("the today date \"2012-03-14\".");
+        assertAnimoResult("all love.", "");
+    }
+
+}
