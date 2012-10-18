a6b4a3f736f353272ef8c75d3ea9b5569b023934
evgeny <gazdovsky@gmail.com>
2012-10-18 18:54:22 +0400

    more tests

--- src/test/java/org/animotron/games/web/HtmlTest.java
+++ src/test/java/org/animotron/games/web/HtmlTest.java
@@ -18,49 +18,41 @@
  *  the GNU Affero General Public License along with Animotron.  
  *  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.animotron.statement.operator;
+package org.animotron.games.web;
 
 import org.animotron.ATest;
-import org.animotron.expression.AnimoExpression;
-import org.animotron.expression.Expression;
 import org.animotron.expression.JExpression;
+import org.animotron.statement.compare.WITH;
+import org.animotron.statement.operator.AN;
+import org.animotron.statement.operator.DEF;
+import org.animotron.statement.query.ANY;
 import org.animotron.statement.query.GET;
-import org.animotron.statement.relation.SHALL;
-import org.junit.Ignore;
+import org.animotron.statement.relation.USE;
 import org.junit.Test;
 
-import static org.animotron.expression.Expression.__;
-import static org.animotron.expression.JExpression.*;
+import static org.animotron.expression.JExpression._;
+import static org.animotron.expression.JExpression.value;
 
 /**
  * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
  * @author <a href="mailto:gazdovsky@gmail.com">Evgeny Gazdovsky</a>
  *
  */
-public class GetNTest extends ATest {
+public class HtmlTest extends ATest {
 
     @Test
-    public void test_000() throws Throwable {
-        testAnimo("def a get 1.");
-        assertAnimoResult("a b", "a b.");
+    public void test_00() throws Throwable {
+        testAnimo("def el \"<\" (id this el) \">\" (an get 2) \"</\" (id this el) \">\".");
+        testAnimo("def p el.");
+        assertStringResult("p 'para'", "<p>para</p>");
     }
 
     @Test
-    public void test_001() throws Throwable {
-        testAnimo("def \\x a get 1.");
-        assertAnimoResult("a b", "a \\x b.");
-    }
-
-    @Test
-    public void test_002() throws Throwable {
-        testAnimo("def a get 1.");
-        assertAnimoResult("a (b) (c)", "a (b) (c).");
-    }
-
-    @Test
-    public void test_003() throws Throwable {
-        testAnimo("def a \\x get 1.");
-        assertAnimoResult("a (b) (c)", "a \\x (b) (c).");
+    public void test_01() throws Throwable {
+        testAnimo("def el \"<\" (id this el) \">\" (an get 2) \"</\" (id this el) \">\".");
+        testAnimo("def ul el.");
+        testAnimo("def li el.");
+        assertStringResult("ul li 1", "<ul><li>1</li></ul>");
     }
 
 }
