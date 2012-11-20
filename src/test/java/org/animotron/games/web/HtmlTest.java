65a0c1e8e1f3b10e425bbeb833555b604fd98ec4
evgeny <gazdovsky@gmail.com>
2012-11-21 00:11:28 +0400

    brain fuck test

--- src/test/java/org/animotron/games/web/HtmlTest.java
+++ src/test/java/org/animotron/games/web/HtmlTest.java
@@ -118,8 +118,8 @@ public class HtmlTest extends ATest {
 
     @Test
     public void test_09() throws Throwable {
-        tAnimo("def \\ '<' (id this) (each (get @ this) (this)) '>' (each (get 1) (this)) '</' (id this) '>'.");
-        assertAnimoResult("\\ p (@ class 'abc') (\\ strong 'foo') (\\ span 'bar')", "<p><stong>foo</strong><span>bar</span></p>");
+        tAnimo("def \\ '<' (id this) (each (get 1) (ptrn (this) (?is @ (' ' (id get @ this 1) '=\\\"' (get (get @ this 1) (get @ this 1)) '\\\"')))) '>' (each (get 1) (this)) '</' (id this) '>'.");
+        assertStringResult("\\ p (@ class 'abc') (\\ strong 'foo') (\\ span (@ style 'display:none') 'bar')", "<p class=\"abc\"><strong>foo</strong><span style=\"display:none\">bar</span></p>");
     }
 
 }
